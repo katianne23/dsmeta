eb46d5d615bac2e2a4fc76126607cde88064000b
katianne <katianne40@gmail.com>
2022-07-12 09:36:37 -0300

    Project Created

---
+++ backend/src/test/java/com/katianne/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katianne.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
